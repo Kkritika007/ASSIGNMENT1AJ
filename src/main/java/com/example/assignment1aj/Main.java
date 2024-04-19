8e9b9eaa8163017b8dd435ed5e0470ecb0deb421
manik17778 <137009112+Kkritika007@users.noreply.github.com>
2024-04-19 00:51:18 -0400

    1

--- src/main/java/com/example/assignment1aj/Main.java
+++ src/main/java/com/example/assignment1aj/Main.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,6 +23,7 @@ public class Main extends Application {
         Scene scene = new Scene((Parent)fxmlLoader.load());
         stage.setTitle("Restaurant Data");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("file:src/main/resources/com/example/assignment1aj/clogo.jpg"));
         stage.show();
     }
 
